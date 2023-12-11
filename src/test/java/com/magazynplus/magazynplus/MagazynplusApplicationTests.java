2d19b6eaae41df9c0a23c36b5eccb52f26300c2c
arcziDudu <artur.augustyn26@gmail.com>
2023-12-11 12:15:02 +0100

    Initial commit

---
+++ src/test/java/com/magazynplus/magazynplus/MagazynplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magazynplus.magazynplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazynplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
